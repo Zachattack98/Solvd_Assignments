f74eb4c5aebed7bddea67e44829060299abd88b1
Zachattack98 <bakugan0498@yahoo.com>
2023-04-11 19:02:44 -0700

    Created java application that displays user information

---
+++ Homework#1/UserInfo.java
@@ -0,0 +1,7 @@
+public class UserInfo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is " + args[0] + ", I live in " + args[1] + " and work as a(n) " + args[2] + ".");
+    }
+    
+}
