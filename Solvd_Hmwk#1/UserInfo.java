f28cb779dba682c2324306de5615dd0fb34d5a92
Zachattack98 <bakugan0498@yahoo.com>
2023-04-19 15:52:05 -0700

    Added condition statements for length of arguments

--- Solvd_Hmwk#1/UserInfo.java
+++ Solvd_Hmwk#1/UserInfo.java
@@ -1,7 +1,18 @@
 public class UserInfo {
     
     public static void main(String[] args) {
+        if(args.length == 0) {
+            System.out.println("Sorry, I don't see snything posted!");
+        }
+        else if(args.length < 3) {
+            System.out.println("Sorry, not enough information provided!");
+        }
+        else if(args.length > 3) {
+            System.out.println("Sorry, too much information provided!");
+        }
+        else {
         System.out.println("Hello World! My name is " + args[0] + ", I live in " + args[1] + " and work as a(n) " + args[2] + ".");
+        }
     }
     
 }
