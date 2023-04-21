a7de90a70721f8cddc12272252bdbce905e120f2
Zachattack98 <bakugan0498@yahoo.com>
2023-04-21 01:57:25 -0700

    Modified to include abstract classes and methods

---
+++ Solvd_Hmwk#3/ComputerRepairService/src/computerrepairservice/ComputerRepairService.java
@@ -0,0 +1,132 @@
+package computerrepairservice;
+
+import java.util.Scanner;
+
+public class ComputerRepairService {
+
+    public static void main(String[] args) {
+        String arrayComponents[] = {"LCD Screen", "Hard Drive", "USB Adapters", "Power Supply Unit", "Cooling Fan"};
+        double dmg[] = {34.7, 87.4, 12.9, 65.1, 95.0};
+        int specs[] = {36, 20, 1, 70, 25};
+        
+        Shop techShop = new Shop();
+        techShop.setName("Gyro Tech Computer Repair Service");
+        techShop.setLocation("Irvine, CA");
+        System.out.println(techShop.toString());
+        System.out.println();
+        
+        Computer comp = new Computer();
+        comp.setDiskSize(4); //GB
+        comp.setRamSize(16); //GB
+        comp.setWeight(4.3); //lbs
+        
+        System.out.println("Does your computer turn on?");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String response = myObj.nextLine();
+        while(!response.equals("Yes") || !response.equals("No")) {
+            System.out.println("Invalid input! Please type answer again.");
+            response = myObj.nextLine();
+            System.out.println();
+        }
+        System.out.println();
+        
+        comp.isWorking(response);
+                
+        if(comp.getWeight() < 5.0){ //Laptop
+            Laptop lap = new Laptop();
+            lap.setLdecision(myObj.nextLine());
+            System.out.println();
+            
+            lap.lcharging(lap.getLdecision());
+        }
+        else if(comp.getWeight() >= 5.0 && comp.getWeight() <= 10.0){ //Home Computer
+            HomeCom home = new HomeCom();
+            home.setHdecision(myObj.nextLine());
+            System.out.println();
+            
+           home.hpower(home.getHdecision());
+        }
+        System.out.println();
+            
+        TotalCost cost = new TotalCost();
+        int total = cost.total;
+            
+        if(response.equals("Yes")) {
+            int tempstat = 0;
+            Diagnostic diag = new Diagnostic();
+
+            diag.numComponents = arrayComponents.length;
+            System.out.println(diag.toString());
+            System.out.println();
+            System.out.println();
+            for(int i = 0; i < arrayComponents.length; i++) {
+                switch(i+1) {
+                    case(1):
+                        Screen myScreen = new Screen();
+                        tempstat = myScreen.isRepairable(dmg[i], tempstat);
+                        tempstat = myScreen.isReplaceable(dmg[i], tempstat);
+                        if(tempstat == 1){
+                            cost.calaculateCost(myScreen.screenPrice(), total);
+                        }
+                        else if(tempstat == 2) {
+                            cost.calaculateCost(70, total); //$70 to replace
+                        }
+                        break;
+                    case(2):
+                        HardDrive myHardDrive = new HardDrive();
+                        tempstat = myHardDrive.isRepairable(dmg[i], tempstat);
+                        tempstat = myHardDrive.isReplaceable(dmg[i], tempstat);
+                        if(tempstat == 1){
+                            cost.calaculateCost(myHardDrive.harddrivePrice(), total);
+                        }
+                        else if(tempstat == 2) {
+                            cost.calaculateCost(60, total);
+                        }
+                        break;
+                     case(3):
+                        AdapterUSB myAdapter = new AdapterUSB();
+                        tempstat = myAdapter.isRepairable(dmg[i], tempstat);
+                        tempstat = myAdapter.isReplaceable(dmg[i], tempstat);
+                        if(tempstat == 1) {
+                            cost.calaculateCost(myAdapter.adapterPrice(), total);
+                        }
+                        else if(tempstat == 2) {
+                            cost.calaculateCost((specs[i] * 20), total);
+                        }
+                        break;
+                    case(4):
+                        PowerUnit myPower = new PowerUnit();
+                        tempstat = myPower.isRepairable(dmg[i], tempstat);
+                        tempstat = myPower.isReplaceable(dmg[i], tempstat);
+                        if(tempstat == 1){
+                            cost.calaculateCost(myPower.powerunitPrice(), total);
+                        }
+                        else if(tempstat == 2) {
+                            cost.calaculateCost(75, total);
+                        }
+                        break;
+                    case(5):
+                        Fan myFan = new Fan();
+                        tempstat = myFan.isRepairable(dmg[i], tempstat);
+                        tempstat = myFan.isReplaceable(dmg[i], tempstat);
+                        if(tempstat == 1){
+                            cost.calaculateCost(myFan.fanPrice(), total);
+                        }
+                        else if(tempstat == 2) {
+                            cost.calaculateCost(40, total);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                
+                diag.result(arrayComponents[i], tempstat);
+                
+            }
+        }
+        System.out.println();
+        System.out.println(cost.toString());
+        System.out.println();
+    }
+
+}
