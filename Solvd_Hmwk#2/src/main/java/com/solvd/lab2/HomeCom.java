f671987584216727d79f76ee36f9829df3007abe
Zachattack98 <bakugan0498@yahoo.com>
2023-05-16 05:24:03 -0700

    Included log.info

--- Solvd_Hmwk#2/src/main/java/com/solvd/lab2/HomeCom.java
+++ Solvd_Hmwk#2/src/main/java/com/solvd/lab2/HomeCom.java
@@ -1,21 +1,23 @@
-package computerrepairservice;
+package com.solvd.lab2;
 
 //import java.util.Properties;
 import java.util.Scanner;
-import computerrepairservice.exception.ComputerNotFoundException;
-import computerrepairservice.interfaces.InitDiagnosis;
+import com.solvd.lab2.exception.ComputerNotFoundException;
+import com.solvd.lab2.interfaces.InitDiagnosis;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HomeCom extends Computer implements InitDiagnosis {
     private String hdecision;
-    
+
     public String getHdecision() {
         return hdecision;
     }
-    
+
     public void setHdecision(String hdecision) {
         this.hdecision = hdecision;
     }
-    
+
     public HomeCom(double weight, int ramSize, int diskSize, String hdecision) throws ComputerNotFoundException {
         super(weight, ramSize, diskSize); //must be first statement in constructor
         //if no viable computer is empty
@@ -26,25 +28,25 @@ public class HomeCom extends Computer implements InitDiagnosis {
         }
         this.hdecision = hdecision;
     }
-    
+
     public HomeCom(String hdecision) {
         this.hdecision = hdecision;
     }
-    
-    @Override 
+
+    @Override
     public void proceed() {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        
+
         //if it is a home computer and powers on, ask customer if you can proceed with diagnosis
         if(weight >= 5.0 && weight <= 10.0 && power.equals("No")){
-            System.out.println("Do you want me to take a closer look?");
+            log.info("Do you want me to take a closer look?");
             hdecision = myObj.nextLine();
             if(hdecision.equals("Yes")) {
-                System.out.println("Great, I'll proceed with the Diagnosis!");
+                log.info("Great, I'll proceed with the Diagnosis!");
                 System.out.println();
             }
             else if(hdecision.equals("No")) {
-                System.out.println("Alright, thanks for stopping by!");
+                log.info("Alright, thanks for stopping by!");
             }
         }
     }
--- Solvd_Hmwk#2/src/main/java/com/solvd/lab2/Laptop.java
+++ Solvd_Hmwk#2/src/main/java/com/solvd/lab2/Laptop.java
@@ -1,21 +1,23 @@
-package computerrepairservice;
+package com.solvd.lab2;
 
 //import java.util.Properties;
 import java.util.Scanner;
-import computerrepairservice.exception.ComputerNotFoundException;
-import computerrepairservice.interfaces.InitDiagnosis;
+import com.solvd.lab2.exception.ComputerNotFoundException;
+import com.solvd.lab2.interfaces.InitDiagnosis;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Laptop extends Computer implements InitDiagnosis {
     private String ldecision;
-    
+
     public String getLdecision() {
         return ldecision;
     }
-    
+
     public void setLdecision(String ldecision) {
         this.ldecision = ldecision;
     }
-    
+
     public Laptop(double weight, int ramSize, int diskSize, String ldecision) throws ComputerNotFoundException {
         super(weight, ramSize, diskSize); //must be first statement in constructor
 
@@ -27,25 +29,25 @@ public class Laptop extends Computer implements InitDiagnosis {
         }
         this.ldecision = ldecision;
     }
-    
+
     public Laptop(String ldecision) {
         this.ldecision = ldecision;
     }
-    
-    @Override 
+
+    @Override
     public void proceed() {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        
+
         //if it is a laptop and powers on, ask customer if you can proceed with diagnosis
         if(weight < 5.0 && power.equals("No")){
-            System.out.println("Do you want me to take a closer look?");
+            log.info("Do you want me to take a closer look?");
             ldecision = myObj.nextLine();
             if(ldecision.equals("Yes")) {
-                System.out.println("Great, I'll proceed with the Diagnosis!");
+                log.info("Great, I'll proceed with the Diagnosis!");
                 System.out.println();
             }
             else if(ldecision.equals("No")) {
-                System.out.println("Alright, thanks for stopping by!");
+                log.info("Alright, thanks for stopping by!");
             }
         }
     }
