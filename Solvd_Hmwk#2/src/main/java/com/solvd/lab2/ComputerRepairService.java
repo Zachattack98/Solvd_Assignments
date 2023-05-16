7228aedd63cd6318d3c5ee6b7931ba947f2039ea
Zachattack98 <bakugan0498@yahoo.com>
2023-05-16 05:25:19 -0700

    Relocated ForEach loop, method calls, etc. from main

--- Solvd_Hmwk#2/src/main/java/com/solvd/lab2/ComputerRepairService.java
+++ Solvd_Hmwk#2/src/main/java/com/solvd/lab2/ComputerRepairService.java
@@ -1,10 +1,10 @@
-package computerrepairservice;
+package com.solvd.lab2;
 
-import computerrepairservice.exception.ShopNotFoundException;
-import computerrepairservice.exception.ComputerNotFoundException;
-import computerrepairservice.exception.ComponentNotFoundException;
-import computerrepairservice.exception.DamageRangeInvalidException;
-import computerrepairservice.enums.Name;
+import com.solvd.lab2.exception.ShopNotFoundException;
+import com.solvd.lab2.exception.ComputerNotFoundException;
+import com.solvd.lab2.exception.ComponentNotFoundException;
+import com.solvd.lab2.exception.DamageRangeInvalidException;
+import com.solvd.lab2.enums.Name;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.ArrayList;
@@ -16,18 +16,24 @@ import org.apache.logging.log4j.Logger;
 
 public class ComputerRepairService{
     static Logger log = LogManager.getLogger(ComputerRepairService.class);
-    
+
     public static void main(String[] args) {
-        
+
+        //initialize objects
+        ServiceShop techShop = null;
+        Computer comp = null;
+        UniqueFileWords unique = null;
+        Diagnostic diag = null;
+
         try {
-            ServiceShop techShop1 = new ServiceShop("Gyro Tech Computer Repair Service", "Irvine, CA");
+            ServiceShop techShop = new ServiceShop("Gyro Tech Computer Repair Service", "Irvine, CA");
             //techShop1.validShop();
-            log.info(techShop1.toString());
+            log.info(techShop.toString());
             System.out.println();
         }catch (ShopNotFoundException se) {
             log.info(se.getMessage());
         }
-         
+
         try {
             Computer comp = new Computer(4.3, 16, 4);
             //comp.validComputer();
@@ -36,88 +42,35 @@ public class ComputerRepairService{
         }catch (ComputerNotFoundException ce) {
             log.info(ce.getMessage());
         }
-        
+
         //Error: Yes, No, and everything ele keep turning out invalid
         //comp.powerOnOff();
-        
+
         //if(comp.power.equals("No")) {
-            //Laptop lap = new Laptop("Yes");
-            //lap.proceed();
-            //HomeCom home = new HomeCom("Yes");
-            //home.proceed();
-            
-            Name cname = Name.SCREEN; //can be any of the constants, only used to initialize cname
-            cname.nameInfo();
-            
-            UniqueFileWords unique = new UniqueFileWords("*Screen, Motherboard, CPU, *HardDrive, Speakers, *USBAdapter, *PowerUnit, *Fan, Memory, DataStorage");
-            unique.uniqueList();
-            
-            Diagnostic diag = new Diagnostic();
-            log.info(diag.toString());
-            System.out.println();   
-                    
-            try {
-                ServiceShop techShop2 = new ServiceShop();
-                
-                //a set collection that contains no duplicates of component names: HashSet
-                Set<String> st = new HashSet<>();
-                
-                Screen screen = new Screen("LCD Screen", 34.7, 36);
-                st.add(screen.nameComponent); //adding name component to hash set
-                HardDrive hd = new HardDrive("Hard Drive", 87.4, 20);
-                st.add(hd.nameComponent);
-                AdapterUSB adapt = new AdapterUSB("USB Adapter(s)", 0.0, 1);
-                st.add(adapt.nameComponent);
-                PowerUnit punit = new PowerUnit("Power Supply Unit", 1.9, 70);
-                st.add(punit.nameComponent);
-                Fan fan = new Fan("Cooling Fan", 54.0, 25);
-                st.add(fan.nameComponent);
+        //Laptop lap = new Laptop("Yes");
+        //lap.proceed();
+        //HomeCom home = new HomeCom("Yes");
+        //home.proceed();
 
-                log.info("List of components that will be diagnosed: " + st);
-                System.out.println();
-                
-                Component[] components = {screen, hd, adapt, punit, fan}; //combine all objects together into a new array
-            
-                //create marker object
-                Marker marker = MarkerManager.getMarker("CLASS");
+        UniqueFileWords unique = new UniqueFileWords("*Screen, Motherboard, CPU, *HardDrive, Speakers, *USBAdapter, *PowerUnit, *Fan, Memory, DataStorage");
+        unique.uniqueList();
 
-                //List for storing the repair status of each component
-                List<Integer> lst = new ArrayList<>();
-    
-                //use for-each loop to implement calculatePrice() method in each sub class of Component()
-                for(Component component: components) {
-                    //validate all conditions for components
-                    component.log(marker);
-                    //if no errors occurred, begin diagnosis
-                    component.determinePrice();
-                    techShop2.calculatePrice(component.price);
-                    //now that we have the status, add it to the list
-                    lst.add(component.statusOfComponent());
-                    component.determineTime();
-                    techShop2.calculateTime(component.time);
-                    
-                    log.info(techShop2.calculatePrice(component.price));
-                    log.info(techShop2.calculateTime(component.time));
-                
-                    diag.printTestNumber();
-                    
-                    diag.recordDamage(component.damage);
-                    diag.recordPrice(component.price);
-                    diag.recordTime(component.time);
-                    
-                    log.info(component.toString());
-                    System.out.println();
-                }
-                
-                diag.listOfStats(st, lst);
-                
-                techShop2.printPriceAndTime();
-            }catch (ComponentNotFoundException | DamageRangeInvalidException dce) { //multicatch
-                log.info(dce.getMessage());
-            }
+        Diagnostic diag = new Diagnostic();
+        log.info(diag.toString());
+        System.out.println();
+
+        try {
+            techShop.outputToMain(diag, techShop.registerComponent(new Screen("LCD Screen", 34.7, 36),
+                                                                  new HardDrive("Hard Drive", 87.4, 20),
+                                                                  new AdapterUSB("USB Adapter(s)", 0.0, 1),
+                                                                  new PowerUnit("Power Supply Unit", 1.9, 70),
+                                                                  new Fan("Cooling Fan", 54.0, 25)););
+        }catch (ComponentNotFoundException | DamageRangeInvalidException dce) { //multicatch
+            log.info(dce.getMessage());
+        }
         //}
         /*else {
-            System.out.println("Alright, thanks for stopping by!");
+            log.info("Alright, thanks for stopping by!");
             System.out.println();
         }*/
     }
--- Solvd_Hmwk#2/src/main/java/com/solvd/lab2/ServiceShop.java
+++ Solvd_Hmwk#2/src/main/java/com/solvd/lab2/ServiceShop.java
@@ -1,21 +1,26 @@
-package computerrepairservice;
+package com.solvd.lab2;
 
-import computerrepairservice.enums.Day;
-import computerrepairservice.exception.ShopNotFoundException;
-import computerrepairservice.lambdas.ToIntFunction;
-import java.util.Scanner;
+import com.solvd.lab2.enums.Day;
+import com.solvd.lab2.exception.ShopNotFoundException;
+import com.solvd.lab2.lambdas.ToIntFunction;
+
+import java.util.*;
+import java.util.function.DoublePredicate;
+import java.util.function.IntConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-//cannot be overriden/modified by any subclasses
-final class ServiceShop {
+//cannot be overridden/modified by any subclasses
+public class ServiceShop extends Component, Diagnostic{
     private String nameShop;
     private String location;
     //ArrayList<Integer> arrPrice = new ArrayList<>(NUM_COMPONENTS); //save all prices found in each subclass in this array
     //ArrayList<Double> arrTime = new ArrayList<>(NUM_COMPONENTS); //save all time intervals found in each subclass in this array
     int totalPrice;
     double totalTime;
-    
     protected Logger shopLogger = LogManager.getLogger();
 
     protected Logger getLogger() {
@@ -25,23 +30,23 @@ final class ServiceShop {
     protected void setLogger(Logger shopLogger) {
         this.shopLogger = shopLogger;
     }
-    
+
     public String getNameShop() {
         return nameShop;
     }
-    
+
     public void setNameShop(String nameShop) {
         this.nameShop = nameShop;
     }
-    
+
     public String getLocation() {
         return location;
     }
-    
+
     public void setLocation(String location) {
         this.location = location;
     }
-    
+
     public ServiceShop(String nameShop, String location) throws ShopNotFoundException {
         //if the shop name is empty
         if(nameShop.equals("")) {
@@ -51,18 +56,18 @@ final class ServiceShop {
         }
         this.location = location;
     }
-    
+
     public ServiceShop() {}
-    
+
     //static block, called before program even runs
     static {
-        System.out.println("Welcome to our Computer Repair shop fine customer!!");
+        shopLogger.info("Welcome to our Computer Repair shop fine customer!!");
     }
-    
+
     @Override public String toString() {
         return("Here at " + nameShop + ", we offer the best computer repair service in " + location + "!");
     }
-    
+
     public int calculatePrice(int price) {
         totalPrice += price;
         return totalPrice;
@@ -72,45 +77,117 @@ final class ServiceShop {
         totalTime += time;
         return totalTime;
     }
-   
-    public void printPriceAndTime() {
+
+    public void printPriceAndTime(ToIntFunction<Double> obj, Function<Integer, Float> factor) {
         Math.round(totalTime); //first round the total time up (0.5-0.9) or down (0.0-0.4)
-        ToIntFunction<Double> obj = t -> (int)(t*1); //convert double to integer.
-                                                     //must multiply by some value to allow conversion.
-        System.out.println("What day is it today? ");
+        obj = t -> (int)(t*1); //convert double to integer.
+        //must multiply by some value to allow conversion.
+        shopLogger.info("What day is it today? ");
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String dayInput = myObj.nextLine();
-        
+
         Day day = null;
         switch(dayInput) {
             case "Monday":
-                day.halfOffDay(Day.MONDAY, totalPrice);
+                day.halfOffDay(factor, Day.MONDAY, totalPrice);
                 break;
             case "Tuesday":
-                day.halfOffDay(Day.TUESDAY, totalPrice);
+                day.halfOffDay(factor, Day.TUESDAY, totalPrice);
                 break;
             case "Wednesday":
-                day.halfOffDay(Day.WEDNESDAY, totalPrice);
+                day.halfOffDay(factor, Day.WEDNESDAY, totalPrice);
                 break;
             case "Thursday":
-                day.halfOffDay(Day.THURSDAY, totalPrice);
+                day.halfOffDay(factor, Day.THURSDAY, totalPrice);
                 break;
             case "Friday":
-                day.halfOffDay(Day.FRIDAY, totalPrice);
+                day.halfOffDay(factor, Day.FRIDAY, totalPrice);
                 break;
             case "Saturday":
-                day.halfOffDay(Day.SATURDAY, totalPrice);
+                day.halfOffDay(factor, Day.SATURDAY, totalPrice);
                 break;
             case "Sunday":
-                day.halfOffDay(Day.SUNDAY, totalPrice);
+                day.halfOffDay(factor, Day.SUNDAY, totalPrice);
                 break;
             default:
-                System.out.println("Invalid day! We'll just assume is a full price day");
+                shopLogger.info("Invalid day! We'll just assume is a full price day");
                 break;
         }
-        
+
         //output overall cost and time it will take.
-        System.out.println("Total cost for repairs: $" + totalPrice + ", and will take " + obj.applyAsInt(totalTime) + " days to finish!");
+        shopLogger.info("Total cost for repairs: $" + totalPrice + ", and will take " + obj.applyAsInt(totalTime) + " days to finish!");
         System.out.println();
     }
+
+    public void outputToMain(Diagnostic diag, Component[] components) {
+        //a set collection that contains no duplicates of component names: HashSet
+        Set<String> st = new HashSet<>();
+
+        Stream<String> setStream = Stream.of(components[0].nameComponent,   //Screen
+                                             components[1].nameComponent,   //Hard Drive
+                                             components[2].nameComponent,   //USB Adapter
+                                             components[3].nameComponent,   //Power Unit
+                                             components[4].nameComponent);  //Cooling Fan
+        //adding name component to hash set
+        setStream.forEach(st::add);
+
+        //Before diagnosis, list names of all components
+        shopLogger.info("List of components that will be diagnosed: ");
+        setStream.forEach(compnt->log.info("{" + compnt + "} "));
+
+        //create marker object
+        //Marker marker = MarkerManager.getMarker("CLASS");
+
+        //List for storing the status of each component
+        List<Integer> lstStat = new ArrayList<>();
+
+        //use for-each loop to implement calculatePrice() method in each sub class of Component()
+        for (Component component : components) {
+            //validate all conditions for components
+            component.log(marker);
+
+            //if no errors occurred, begin diagnosis
+            //First, price
+            component.determinePrice(IntConsumer mul = new IntConsumer);
+            calculatePrice(component.price);
+
+            //now that we have the status, add it to the list
+            lstStat.add(component.statusOfComponent(DoublePredicate dp = new DoublePredicate));
+
+            //Second, time
+            component.determineTime();
+            calculateTime(component.time);
+
+            diag.printTestNumber();
+
+            diag.recordDamage(component.damage);
+            diag.recordPrice(component.price);
+            diag.recordTime(component.time);
+
+            shopLogger.info(component.toString());
+            System.out.println();
+        }
+        diag.listOfStats(st, lstStat, ObjIntConsumer<List<Integer>> consumerObj = new ObjIntConsumer<List<Integer>>);
+
+        //print lists of each individual price and time (minus 0.0 time)
+        components.lstPrice.stream().sorted().collect(Collectors.toSet());
+        shopLogger.info(components.lstPrice);
+        components.lstTime.stream().sorted().filter((Time t) -> ((t.getTime > 0.0)).collect(Collectors.toSet());
+        shopLogger.info(components.lstTime);
+
+        //print total price and time
+        printPriceAndTime(ToIntFunction<Double> obj = new ToIntFunction<Double>, Function<Integer, Float> factor = new Function<Integer, Float>);
+    }
+
+    static Component[] registerComponent(Screen screen, HardDrive hd, AdapterUSB adapt, PowerUnit punit, Fan fan) {
+        Component[] components = new Component[5];
+        components[0] = screen;
+        components[1] = hd;
+        components[2] = adapt;
+        components[3] = punit;
+        components[4] = fan;
+        return components;
+    }
+
+
 }
