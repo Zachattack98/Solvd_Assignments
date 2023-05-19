f7931edc488a7efe95eb530819cf6383bc0e1391
Zachattack98 <bakugan0498@yahoo.com>
2023-05-19 06:40:36 -0700

    reconfigured registerComponent method calls

--- Solvd_Hmwk#2/src/main/java/com/solvd/lab2/ComputerRepairService.java
+++ Solvd_Hmwk#2/src/main/java/com/solvd/lab2/ComputerRepairService.java
@@ -4,43 +4,39 @@ import com.solvd.lab2.exception.ShopNotFoundException;
 import com.solvd.lab2.exception.ComputerNotFoundException;
 import com.solvd.lab2.exception.ComponentNotFoundException;
 import com.solvd.lab2.exception.DamageRangeInvalidException;
-import com.solvd.lab2.enums.Name;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
+
+import java.io.IOException;
+
+import com.solvd.lab2.uniquewordcounter.UniqueFileWords;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ComputerRepairService{
-    static Logger log = LogManager.getLogger(ComputerRepairService.class);
+    private static final Logger mainlog = LogManager.getLogger(ComputerRepairService.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //initialize objects
         ServiceShop techShop = null;
         Computer comp = null;
-        UniqueFileWords unique = null;
         Diagnostic diag = null;
 
         try {
-            ServiceShop techShop = new ServiceShop("Gyro Tech Computer Repair Service", "Irvine, CA");
+            techShop = new ServiceShop("Gyro Tech Computer Repair Service", "Irvine, CA");
             //techShop1.validShop();
-            log.info(techShop.toString());
+            mainlog.info(techShop.toString());
             System.out.println();
         }catch (ShopNotFoundException se) {
-            log.info(se.getMessage());
+            mainlog.info(se.getMessage());
         }
 
         try {
-            Computer comp = new Computer(4.3, 16, 4);
+            comp = new Computer(4.3, 16, 4);
             //comp.validComputer();
             comp.printComputerInfo();
             System.out.println();
         }catch (ComputerNotFoundException ce) {
-            log.info(ce.getMessage());
+            mainlog.info(ce.getMessage());
         }
 
         //Error: Yes, No, and everything ele keep turning out invalid
@@ -52,21 +48,21 @@ public class ComputerRepairService{
         //HomeCom home = new HomeCom("Yes");
         //home.proceed();
 
-        UniqueFileWords unique = new UniqueFileWords("*Screen, Motherboard, CPU, *HardDrive, Speakers, *USBAdapter, *PowerUnit, *Fan, Memory, DataStorage");
-        unique.uniqueList();
-
-        Diagnostic diag = new Diagnostic();
-        log.info(diag.toString());
+        diag = new Diagnostic();
+        mainlog.info(diag.toString());
         System.out.println();
 
         try {
-            techShop.outputToMain(diag, techShop.registerComponent(new Screen("LCD Screen", 34.7, 36),
-                                                                  new HardDrive("Hard Drive", 87.4, 20),
-                                                                  new AdapterUSB("USB Adapter(s)", 0.0, 1),
-                                                                  new PowerUnit("Power Supply Unit", 1.9, 70),
-                                                                  new Fan("Cooling Fan", 54.0, 25)););
+
+            techShop.registerComponent(new Screen("LCD Screen", 34.7, 36));
+            techShop.registerComponent(new HardDrive("Hard Drive", 87.4, 20));
+            techShop.registerComponent(new AdapterUSB("USB Adapter(s)", 0.0, 1));
+            techShop.registerComponent(new PowerUnit("Power Supply Unit", 1.9, 70));
+            techShop.registerComponent(new Fan("Cooling Fan", 54.0, 25));
+
+            techShop.outputToMain(diag);
         }catch (ComponentNotFoundException | DamageRangeInvalidException dce) { //multicatch
-            log.info(dce.getMessage());
+            mainlog.info(dce.getMessage());
         }
         //}
         /*else {
