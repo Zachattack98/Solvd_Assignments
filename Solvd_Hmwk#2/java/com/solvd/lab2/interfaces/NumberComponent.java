bb39f72158bc0a22543331284e0ec44bb494e628
Zachattack98 <bakugan0498@yahoo.com>
2023-05-30 11:22:22 -0700

    Included abstract method for Diagnostic and Component

--- Solvd_Hmwk#2/java/com/solvd/lab2/interfaces/NumberComponent.java
+++ Solvd_Hmwk#2/java/com/solvd/lab2/interfaces/NumberComponent.java
@@ -1,6 +1,15 @@
 package com.solvd.lab2.interfaces;
 
+import com.solvd.lab2.lambdas.ObjIntConsumer;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public interface NumberComponent {
     //total number of components
     public static int NUM_COMPONENTS = 5;
+
+    public void printTestNumber();
 }
